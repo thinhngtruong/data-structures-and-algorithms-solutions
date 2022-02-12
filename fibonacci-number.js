66bcd819b50ae23be198b2580334d5de5f9c6e0e
Thomas <thomas@boostcommerce.net>
2022-02-12 15:29:04 +0700

    Update solution fibonacci O(1) space

--- fibonacci-number.js
+++ fibonacci-number.js
@@ -1,7 +1,7 @@
 /* 
-  Problem: https://leetcode.com/problems/fibonacci-number/
-  Space: O(n)  
-  Time: O(n)
+ * Problem: https://leetcode.com/problems/fibonacci-number/
+ * Space: O(n)  
+ * Time: O(n)
 */
 var fib = function (n) {
   let f = new Array(n + 1);
@@ -11,4 +11,22 @@ var fib = function (n) {
     f[i] = f[i - 2] + f[i - 1]
   }
   return f[n]
+};
+
+/**
+ * Two pointer approach
+ * Space: O(1)  
+ * Time: O(n)
+ */
+ var fib = function(n) {
+  if(n <= 1) return n
+  let prev1 = 1;
+  let prev2 = 0;
+  let cur
+  for(let i=2; i <= n; i++) {
+      cur = prev1 + prev2;
+      prev2 = prev1;
+      prev1 = cur;
+  }
+  return cur
 };
