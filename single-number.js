275223132bcb3b682634a5961174f62dbbf61b70
Thinh Nguyen <thinh.ngtruong@gmail.com>
2022-03-01 00:00:07 +0700

    Single number

--- single-number.js
+++ single-number.js
@@ -12,3 +12,20 @@ var singleNumber = function(nums) {
     }
     return result;
 };
+
+/**
+ * Map solution
+ * Time: O(n)
+ * Space: O(n)
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    let map = {};
+    for(let num of nums) {
+        map[num] = map[num] + 1 || 1;
+    }
+    for(let key in map) {
+        if(map[key] === 1) return key;
+    }
+};
