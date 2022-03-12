4aa755a41569c96e6b5f5b354c762fec686491cc
Thinh Nguyen <thinh.ngtruong@gmail.com>
2022-03-13 00:09:34 +0700

    Invert binary tree

---
+++ invert-binary-tree.js
@@ -0,0 +1,50 @@
+/**
+ * Problem: https://leetcode.com/problems/invert-binary-tree/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+// Recursion
+function invertTree(root) {
+  if (root == null) return root;
+  [root.left, root.right] = [invertTree(root.right), invertTree(root.left)];
+  return root;
+}
+
+// DFS
+function invertTree(root) {
+  const stack = [root];
+
+  while (stack.length) {
+    const n = stack.pop();
+    if (n != null) {
+      [n.left, n.right] = [n.right, n.left];
+      stack.push(n.left, n.right);
+    }
+  }
+
+  return root;
+}
+
+// BFS
+function invertTree(root) {
+  const queue = [root];
+
+  while (queue.length) {
+    const n = queue.shift();
+    if (n != null) {
+      [n.left, n.right] = [n.right, n.left];
+      queue.push(n.left, n.right);
+    }
+  }
+
+  return root;
+}
